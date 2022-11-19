b3912726881e3f0e35921b738b0bdb99dcc79c07
Pranita Waghdhare <pni.pranita@gmail.com>
2022-11-19 20:54:03 +0530

    Add files via upload

---
+++ Muscular Fitness Hub/js/script.js
@@ -0,0 +1,18 @@
+function registerForm() {
+    alert("Please Fill The Registration Form");
+}
+function btnSubscribe() {
+    eid = document.getElementById("eid").value;
+    if (eid == null || eid == "") {
+        alert("Please Enter Valid Email");
+    } else {
+        alert("Thank You For Subscribing To Our Newsletter.");
+    }
+}
+function submitForm() {
+    window.alert("Thank You For Register With Us!");
+    window.alert("We'll Contact You.");
+}
+function btnSave() {
+    alert("Please wait for the reply.")
+}
